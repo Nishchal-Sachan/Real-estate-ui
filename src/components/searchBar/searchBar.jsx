c4b1b1b9ae4b544ea3852820fdf25e9bc4e9375a
Nishchal-Sachan <sachannishchal@gmail.com>
2024-11-16 17:12:53 +0530

    fixed search bar

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -26,9 +26,11 @@ function SearchBar() {
                 <input type="text" name="location" placeholder="City Location" />
                 <input type="number" name="minPrice" min={0} max={1000000} placeholder="Min Price" />
                 <input type="number" name="maxPrice" min={0} max={1000000} placeholder="Max Price" />
-                <button>
-                    <img src="./search.png" alt="" />
-                </button>
+                <Link to="/list">
+                    <button>
+                        <img src="./search.png" alt="" />
+                    </button>
+                </Link>
             </form>
 
         </div>
