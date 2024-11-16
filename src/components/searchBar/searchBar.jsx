8875bfa1bf8c3d659866e978dc09bcd40c3207ac
Nishchal-Sachan <sachannishchal@gmail.com>
2024-11-16 17:14:28 +0530

    fixed searchbar

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react"
-import "./searchBar.scss"
+import { useState } from "react";
+import "./searchBar.scss";
+import { Link } from "react-router-dom";
+
 
 const types = ["buy", "rent"];
 
